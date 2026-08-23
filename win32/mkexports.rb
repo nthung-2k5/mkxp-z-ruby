7cb17c971243ad461900bcab52d221c453506831
nthung-2k5 <hungthitkhia@gmail.com>
2026-08-23 19:51:40 +0700

    Add compilation for Visual Studio 2026 and MSVC 19.5x

--- win32/mkexports.rb
+++ win32/mkexports.rb
@@ -113,6 +113,7 @@ class Exports::Mswin < Exports
         end
         case filetype
         when /OBJECT/, /LIBRARY/
+          next if (/^ .*\(pick any\)$/ =~ l)...true
           next if /^[[:xdigit:]]+ 0+ UNDEF / =~ l
           next unless /External/ =~ l
           next if /(?:_local_stdio_printf_options|v(f|sn?)printf(_s)?_l)\Z/ =~ l
